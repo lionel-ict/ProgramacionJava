a7cd42ae93c3c4bdf752bf076f02df98dce4a2e6
Lionel Tarazon <lionel.ta@gmail.com>
2023-04-18 13:53:38 +0200

    Pequeños cambios en HolaMundo

--- Pruebas/HolaMundo.java
+++ Pruebas/HolaMundo.java
@@ -4,7 +4,10 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo!!!!!");
+        System.out.println("Hola mundo!!!!!");
+        System.out.println("Hola mundo!!!!!");
+        System.out.println("Hola mundo!!!!!");
 
     }
 
